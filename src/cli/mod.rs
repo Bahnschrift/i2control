8e3e9fe21e55b9480eecc75453dee3636603ebad
Bahnschrift <kobeckett+git@gmail.com>
2025-05-13 17:57:29 +1000

    Decent CLI framework setup

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,2 +1,78 @@
-pub mod args;
 pub mod rgb;
+
+use clap::{Parser, Subcommand};
+use rgb::Rgb;
+
+// TODO: Change this to automatically detect connected devices matching known VID/PIDs.
+//       This may require some sort of udev rule generation, at least on linux targets.
+
+/// Model I2 Wireless Vendor ID
+const I2_WL_VID: u16 = 0x93A;
+/// Model I2 Wireless 2.4GHz wireless mode Product ID
+const I2_WL_PID: u16 = 0x821D;
+
+/// CLI Arguments
+#[derive(Debug, Parser)]
+#[command(
+    name = "i2control",
+    about = "CLI replacement for Glorious Control for the Model I2 Wireless"
+)]
+pub struct Args {
+    /// Vendor ID
+    #[arg(default_value_t = I2_WL_VID)]
+    pub vid: u16,
+
+    /// Product ID
+    #[arg(default_value_t = I2_WL_PID)]
+    pub pid: u16,
+
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+/// Command types
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    #[command(short_flag = 'p')]
+    #[command(about = "Set the polling rate")]
+    PollingRate { polling_rate: u16 },
+
+    #[command(subcommand, subcommand_help_heading = "RGB Modes")]
+    #[command(name = "rgb", short_flag = 'l')]
+    #[command(about = "Configure RGB lighting settings")]
+    LightingMode(LightingMode),
+
+    #[command(short_flag = 'b')]
+    #[command(about = "Get the current battery percentage")]
+    Battery {},
+
+    #[command(short_flag = 'd')]
+    #[command(about = "Configure DPI profiles")]
+    Dpi {},
+}
+
+/// Lighting effects corresponding to the options in Glorious Core.
+/// Variants with an RGB value require a specified custom colour.
+/// Casting to `u8` gives the correct byte for HID reports.
+#[repr(u8)]
+#[derive(Debug, Subcommand)]
+pub enum LightingMode {
+    Off,
+
+    Glorious,
+
+    SeamlessBreathing,
+
+    // TODO: Make help for rgb command show that these modes require a colour
+    Breathing { col: Rgb },
+
+    SingleColour { col: Rgb },
+
+    BreathingSingleColour { col: Rgb },
+
+    Tail,
+
+    Rave { col: Rgb },
+
+    Wave,
+}
--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,12 @@ mod lighting;
 mod util;
 
 use clap::Parser;
-use cli::args::Args;
+use cli::Args;
 use std::error::Error;
 
-use hidapi::{HidApi, HidDevice};
+use hidapi::{DeviceInfo, HidApi};
 
-fn set_polling_rate(mouse: &HidDevice, rate: u16) -> Result<(), Box<dyn Error>> {
-    match rate {
-        1000 => {
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/1"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/2"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/3"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/4"))?;
-        }
-        500 => {
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/1"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/2"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/3"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/4"))?;
-        }
-        250 => {
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/1"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/2"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/3"))?;
-            mouse.send_feature_report(get_dump!("../data/polling/1000Hz/4"))?;
-        }
-        125 => todo!(),
-        _ => return Err("Invalid polling rate. Must be one of 125, 250, 500, 1000".into()),
-    }
-
-    Ok(())
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
-    let api = HidApi::new()?;
-
-    let mouse = api.open(args.vid, args.pid)?;
-
-    let info = mouse.get_device_info()?;
+fn print_device_info(info: &DeviceInfo) {
     println!(
         "{}: {} ({}:{})",
         info.manufacturer_string().unwrap_or("Unknown"),
@@ -49,9 +16,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         info.vendor_id(),
         info.product_id()
     );
+}
 
-    if let Some(rate) = args.polling_rate {
-        set_polling_rate(&mouse, rate)?;
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let api = HidApi::new()?;
+    
+    let mouse = api.open(args.vid, args.pid)?;
+    let info = mouse.get_device_info()?;
+    print_device_info(&info);
+    
+    match args.command {
+        _ => todo!(),
     }
 
     Ok(())
